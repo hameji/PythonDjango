3ea32804ccc72c6a6c04bc16232c23be1e13e889
Hajime Taniguchi <hameji@gmail.com>
2021-01-08 00:56:17 +0900

    全タスク完了

---
+++ task02/tutorial.py
@@ -0,0 +1,28 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+options = Options()
+options.add_argument('--disable-gpu');
+options.add_argument('--disable-extensions');
+options.add_argument('--proxy-server="direct://"');
+options.add_argument('--proxy-bypass-list=*');
+options.add_argument('--start-maximized');
+# options.add_argument('--headless'); # ※ヘッドレスモードを使用する場合、コメントアウトを外す
+
+DRIVER_PATH = '{{ WebDriverのパスを指定(絶対でも相対パスでも可) }}'
+# DRIVER_PATH = '/Users/Kenta/Desktop/Selenium/chromedriver' # ローカル
+# DRIVER_PATH = '/app/.chromedriver/bin/chromedriver'        # heroku
+
+# ブラウザの起動
+driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
