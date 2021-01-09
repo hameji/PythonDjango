f1971f9269d804186e49dfb2ff5014b57c9012c0
Hajime Taniguchi <hameji@gmail.com>
2021-01-10 00:34:40 +0900

    課題４を終了

--- task03/search.py
+++ task03/search.py
@@ -1,21 +1,21 @@
 import pandas as pd
-import eel
 
 ### デスクトップアプリ作成課題
 def kimetsu_search(word):
     # 検索対象取得
     df=pd.read_csv("./source.csv")
     source=list(df["name"])
-
     # 検索
     if word in source:
-        print("『{}』はあります".format(word))
+        return True
     else:
-        print("『{}』はありません".format(word))
-        # 追加
-        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        #if add_flg=="1":
-        source.append(word)
+        return False
+
+def add_to_kimetsu(word):
+    # 検索対象取得
+    df=pd.read_csv("./source.csv")
+    source=list(df["name"])
+    source.append(word)
     
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
--- task03/view.py
+++ task03/view.py
@@ -8,7 +8,19 @@ size=(700,600)
 
 @ eel.expose
 def kimetsu_search(word):
-    search.kimetsu_search(word)
+    result = search.kimetsu_search(word)
+    if result:
+        eel.display_result(f"『{word}』は保存されいてます。")    
+    else:
+        eel.display_result(f"『{word}』は保存されていません。")
+        eel.ask_if_append(word)
+
+@ eel.expose
+def add_to_kimetsu(word):
+    search.add_to_kimetsu(word)
+    eel.display_result(f"『{word}』をファイルに保存しました。")
+
+
     
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
